b5ab04619cdf38d5593b8e5643c0c90a2aa1ea28
Jesper Eriksson <je223gs@student.lnu.se>
2019-01-23 17:45:40 +0100

    Testing commit

--- src/first.java
+++ src/first.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main () {
+public class first {
+    public static void main (String[] args) {
+    System.out.println("hello world!");
     System.out.println("hello world!");
     }
   }
